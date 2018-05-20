39f77776c258854a1e4dc44a2bd1d4cbdccf4b59
Cristian Ojeda <cristianeddie@hotmail.cl>
2018-05-20 15:40:31 -0400

    Modelo preliminar de pagina de inicio
    Se agregó el modelo preliminar para ejecutar las pruebas de muestreo de elementos de post en la pagina web

--- Code_page/mysite/blog/views.py
+++ Code_page/mysite/blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+#El punto después de from indica el directorio actual o la aplicación actual.
+#Como views.py y models.py están en el mismo directorio, simplemente usamos . y el
+#nombre del archivo (sin .py). Ahora importamos el nombre del modelo (Post).
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(fecha_creacion__lte=timezone.now()).order_by('fecha_creacion')
+    return render(request, 'blog/post_list.html', {'posts': posts})
