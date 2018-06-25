68666be2bd9fd92c197e5af9688525c50ad2f5f7
eleazar vasquez <ele.vasquez.f@gmail.com>
2018-06-25 17:09:02 -0400

    buscador simple

--- Code_page/mysite/blog/urls.py
+++ Code_page/mysite/blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
-from . import views
+from . import views 
+
 
 urlpatterns = [
-        url('^$', views.post_list),
+        url('^$', views.post_list, name = "buscar"),
 ]
