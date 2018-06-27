0b41abad72ef38bd996eeb172f5170231114ff58
Unknown <cristianeddie@hotmail.cl>
2018-06-27 18:16:33 -0400

    Buscador 2.0 y link
    ahora el buscador, busca estando dentro del post, y los links se redireccionan bien

--- Code_page/mysite/blog/views.py
+++ Code_page/mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 #El punto después de from indica el directorio actual o la aplicación actual.
 #Como views.py y models.py están en el mismo directorio, simplemente usamos . y el
 #nombre del archivo (sin .py). Ahora importamos el nombre del modelo (Post).
@@ -8,10 +9,14 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     if request.method == 'POST':
-    	print (request.POST.get('entrada'))
     	posts = reversed(Post.objects.filter(titulo__contains=request.POST.get('entrada')))
-    else:	
+    else:
     	posts = reversed(Post.objects.filter(fecha_creacion__lte=timezone.now()).order_by('fecha_creacion'))
     return render(request, 'blog/post_list.html', {'posts': posts})
-
-
+def post_detail(request, pk):
+    if request.method == 'POST':
+        posts = reversed(Post.objects.filter(titulo__contains=request.POST.get('entrada')))
+        return render(request, 'blog/post_list.html', {'posts': posts})
+    else:
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
